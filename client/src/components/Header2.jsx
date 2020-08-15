382e03e6ef114190c6e3c3e73ff151565b1e8898
phcreery <phcreery@gmail.com>
2020-08-15 17:02:27 -0500

    fix submenu scroll bug

--- client/src/components/Header2.jsx
+++ client/src/components/Header2.jsx
@@ -13,9 +13,9 @@ export default class MainHeader extends Component {
 	render() {
 		return (
 			<div>
-				<Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+				<Header style={{ position: 'fixed', width: '100%' }}>
           {/* <div className="logo" /> */}
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} getPopupContainer={node => node.parentNode}>
             <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
