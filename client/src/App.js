1aafdd4818b64555b3fccc64d9225858e9e57a42
phcreery <phcreery@gmail.com>
2020-08-16 15:00:04 -0500

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
 import './css/App.css'
+// import { BrowserRouter as Router, Route, Link, Switch, useHistory } from "react-router-dom";
 
 // Import Main React Component
 import Main from './components/Main'
