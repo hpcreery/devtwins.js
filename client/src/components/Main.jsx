4ab44d8cf4ed46e307bcd76c1993873bdef85367
phcreery <phcreery@gmail.com>
2020-09-07 18:58:58 -0500

    footer

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -10,7 +10,7 @@ import FrontPage from './pages/FrontPage'
 import SearchPage from './pages/SearchPage'
 
 // UI Elements
-import { Layout, Affix, Button, Row, Col, BackTop } from 'antd'
+import { Layout, Affix, Button, Row, Col, BackTop, Dropdown, Menu } from 'antd'
 import {
   MailOutlined,
   AppstoreOutlined,
@@ -19,16 +19,45 @@ import {
   GithubOutlined,
   YoutubeOutlined,
   InstagramOutlined,
+  DownOutlined,
 } from '@ant-design/icons'
 import { useThemeSwitcher } from 'react-css-theme-switcher'
 
 const { Header, Footer, Sider, Content } = Layout
+
+const igmenu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="https://www.instagram.com/peyton_creery/?hl=en">peyton_creery</Menu.Item>
+    <Menu.Item key="https://www.instagram.com/_huntercreery_/?hl=en">_huntercreery_</Menu.Item>
+  </Menu>
+);
+
+const githubmenu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="https://github.com/phcreery">phcreery</Menu.Item>
+    <Menu.Item key="https://github.com/hpcreery">hpcreery</Menu.Item>
+  </Menu>
+);
+
+const ytmenu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="https://www.youtube.com/channel/UCfmeNqWuXu1V1EgYaPNiywg">phcreery</Menu.Item>
+    <Menu.Item key="https://www.youtube.com/channel/UCNjaQzmH4ZDepLGT77JI_lA">hpcreery</Menu.Item>
+  </Menu>
+);
+
+function handleMenuClick(e) {
+  console.log('click', e.key);
+  var win = window.open(e.key, '_blank');
+  win.focus();
+}
 
 // Main Class
 export default class Main extends Component {
   constructor(props) {
     super(props)
     document.documentElement.dataset.scroll = 0
+    this.contentRef = React.createRef()
   }
 
   state = {}
@@ -76,28 +105,40 @@ export default class Main extends Component {
       <Router>
         <Layout className='Site-Layout'>
           <SiteHeader />
-          <Content className='Content'>
+          <Content className='Content' ref={this.contentRef}>
             <Route path='/' exact component={FrontPage} />
             <Route path='/search' exact component={SearchPage} />
             <Route path='/:category/:page' component={PageHandler} />
+            {/* <BackTop> */}
+              <div className="Back-Up">UP</div>
+            {/* </BackTop> */}
           </Content>
-          <BackTop>
-            <div className="Back-Up">UP</div>
-          </BackTop>
+          
           <Footer>
             <Row justify='space-between'>
-              <Col span={20}>Created by Hunter & Peyton Creery</Col>
+              <Col xs={10} sm={12} md={15} lg={20} xl={20}>Created by Hunter & Peyton Creery</Col>
               {/* <Col> */}
               <Col>
-                <GithubOutlined />
+                {/* <Button type="text" icon={<GithubOutlined />}></Button> */}
+                <Dropdown overlay={githubmenu}>
+                  <Button type="text" icon={<GithubOutlined />}><DownOutlined /></Button>
+                </Dropdown>
               </Col>
               <Col>
-                <YoutubeOutlined />
+                {/* <Button type="text" icon={<YoutubeOutlined />}></Button> */}
+                {/* <YoutubeOutlined /> */}
+                <Dropdown overlay={ytmenu}>
+                  <Button type="text" icon={<YoutubeOutlined />}><DownOutlined /></Button>
+                </Dropdown>
               </Col>
               <Col>
-                <InstagramOutlined />
+                {/* <Button type="text" icon={<InstagramOutlined />}></Button> */}
+                {/* <InstagramOutlined /> */}
+                <Dropdown overlay={igmenu} >
+                  <Button type="text" icon={<InstagramOutlined />}><DownOutlined /></Button>
+                </Dropdown>
               </Col>
-              Social
+              {/* Social */}
               {/* </Col> */}
             </Row>
             {/* Created by Hunter & Peyton Creery */}
